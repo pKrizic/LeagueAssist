5b7489b6e2a2f9ec5a04a22d5d19538107c6e2b3
Filip <Filip@FILIP-PC>
2017-01-07 16:13:48 +0100

    Added classes for NHibernate mapping.

---
+++ LeagueAssist/Mappings/OrganizationMap.cs
@@ -0,0 +1,22 @@
+﻿using FluentNHibernate.Mapping;
+using LeagueAssist.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeagueAssist.Mappings
+{
+    public class OrganizationMap : ClassMap<Organization>
+    {
+        public OrganizationMap()
+        {
+            Id(x => x.Id);
+            Map(x => x.Name);
+            References(x => x.User);
+            References(x => x.City);
+
+        }
+    }
+}
---
+++ LeagueAssist/Mappings/RoleMap.cs
@@ -0,0 +1,19 @@
+﻿using FluentNHibernate.Mapping;
+using LeagueAssist.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeagueAssist.Mappings
+{
+    public class RoleMap: ClassMap<Role>
+    {
+        RoleMap()
+        {
+            Id(x => x.Id);
+            Map(x => x.Name);
+        }
+    }
+}
