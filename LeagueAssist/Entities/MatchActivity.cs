003d7c8d0edd6d2187df0adc10ec3b9d1e7811ed
kokica00 <koka_pozic@hotmail.com>
2017-01-08 10:54:52 +0100

    Add files via upload

---
+++ LeagueAssist/Entities/MatchActivity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeagueAssist.Entities
+{
+   public class MatchActivity
+    {
+        public virtual int Id { get; protected set; }
+        public virtual Match Match { get; set; }
+        public virtual Person Player { get; set; }
+        public virtual PlayerPerformance Performance { get; set; }
+        public virtual int MatchMinute { get; set; }
+
+    }
+}
