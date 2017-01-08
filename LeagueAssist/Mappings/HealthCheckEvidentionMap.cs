ec88fca691a3a3d2d685c2801bc60d0e6191e8c5
Zvonimir <zgracak@gmail.com>
2017-01-08 10:19:47 +0100

    Nhibernate

---
+++ LeagueAssist/Mappings/HealthCheckEvidentionMap.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentNHibernate.Mapping;
+using LeagueAssist.Entities;
+
+namespace LeagueAssist.Mappings
+{
+    class HealthCheckEvidentionMap : ClassMap<HealthCheckEvidention>
+    {
+        public HealthCheckEvidentionMap()
+        {
+            Id(x => x.Id);
+            References(x => x.Player);
+            Map(x => x.fromDate);
+            Map(x => x.toDate);
+            Table("HealthCheckEvidention");
+        }
+    }
+}
