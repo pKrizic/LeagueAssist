b86197e66cc7256f7f5eae6962a54ff5d56b8b8e
LIJAPC\lija <ivan.liovic1@hotmail.com>
2017-01-11 18:27:54 +0100

    Dodana metoda za dohvat performansa igrača.

---
+++ RestApi/Controllers/PlayerPerformanceController.cs
@@ -0,0 +1,54 @@
+﻿using LeagueAssist;
+using RestApi.Responses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace RestApi.Controllers
+{
+    public class PlayerPerformanceController : ApiController
+    {
+        // GET: api/PlayerPerformance
+        public IHttpActionResult Get()
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            var clas = new Class1();
+            var response = clas.GetAllPlayerPerformance();
+            var message = new PlayerPerformanceResponse();
+            if (response.Count > 0 && response != null)
+            {
+                message.listResponse = response;
+                return Ok(message);
+            }
+            else
+            {
+                return Ok(message);
+            }
+        }
+
+        // GET: api/PlayerPerformance/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/PlayerPerformance
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/PlayerPerformance/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/PlayerPerformance/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
