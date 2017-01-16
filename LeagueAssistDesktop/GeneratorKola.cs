c7712ea0255f430f82c29f2d8c999f138b48b522
LIJAPC\lija <ivan.liovic1@hotmail.com>
2017-01-16 19:16:26 +0100

    Dodan generator kola, ali ne radi još kako spada, roundRobin princip

--- LeagueAssistDesktop/GeneratorKola.cs
+++ LeagueAssistDesktop/GeneratorKola.cs
@@ -16,5 +16,61 @@ namespace LeagueAssistDesktop
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            List<int> dohvatiBrojEkipaULigi = new List<int>(){ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}; //id-jevi ekipa, iz orgcompetition gdje sezona = odabrana i natjecanje = odabrano
+            var list = RoundRobinScheduler(dohvatiBrojEkipaULigi);
+            // spremiti u bazu podatke o utakmicama
+        }
+
+        private Dictionary<int, List<int[]>> RoundRobinScheduler (List<int> ids)
+        {
+            Dictionary<int, List<int[]>> dict = new Dictionary<int, List<int[]>>();
+            List<int[]> listaParova = new List<int[]>();
+            if ((ids.Count % 2) == 1)
+                ids.Add(0);
+
+            int brojKola = (ids.Count - 1);
+            int halfSize = ids.Count / 2;
+
+            List<int> teams = new List<int>();
+            teams.AddRange(ids.Skip(halfSize).Take(halfSize));
+            teams.AddRange(ids.Skip(1).Take(halfSize - 1).ToArray().Reverse());
+            int teamsSize = teams.Count;
+
+            for (int day = 0; day < brojKola; day++)
+            {
+                dict.Add(day + 1, null);
+                int teamIdx = day % teamsSize;
+                if ((day % 2) == 1)
+                {
+                    int[] ekipe = { teams[teamIdx], ids[0] };
+                    listaParova.Add(ekipe);
+                    for (int idx = 1; idx < halfSize; idx++)
+                    {
+                        int firstTeam = (day + idx) % teamsSize;
+                        int secondTeam = (day + teamsSize - idx) % teamsSize;
+                        int[] ekip = { firstTeam, secondTeam };
+                        listaParova.Add(ekip);
+                    }
+                }
+                else
+                {
+                    int[] ekipe = { ids[0], teams[teamIdx] };
+                    listaParova.Add(ekipe);
+                    for (int idx = 1; idx < halfSize; idx++)
+                    {
+                        int firstTeam = (day + teamsSize - idx) % teamsSize;
+                        int secondTeam = (day + idx) % teamsSize;
+                        int[] ekip = { firstTeam, secondTeam };
+                        listaParova.Add(ekip);
+                    }
+                }
+                dict[day + 1] = listaParova;
+                listaParova = new List<int[]>();
+            }
+            return dict;
+        }
     }
 }
