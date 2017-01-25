b788df157b5e37ab3dd7a77c61d8533f1a0e740b
LIJAPC\lija <ivan.liovic1@hotmail.com>
2017-01-25 14:01:01 +0100

    Ispravak merga

--- LeagueAssist/CompetitionRepository.cs
+++ LeagueAssist/CompetitionRepository.cs
@@ -6,7 +6,21 @@ using System.Threading.Tasks;
 
 namespace LeagueAssist
 {
-    class CompetitionRepository
+    public class CompetitionRepository
     {
+        //public List<Fixture> GetFixtures()
+        //{
+        //    var result = new List<Fixture>();
+        //    var clas = new Class1();
+        //    using (var session = clas.OpenSession())
+        //    {
+        //        using (var transaction = session.BeginTransaction())
+        //        {
+        //            result = (List<Fixture>)session.QueryOver<Fixture>().List<Fixture>();
+        //            transaction.Commit();
+        //        }
+        //    }
+        //    return result;
+        //}
     }
 }
