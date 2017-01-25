ec2da04612cfa9e605cf46ca247e628b20b43245
FILIP-PC\Filip <filip.jurcic@fer.hr>
2017-01-25 13:51:56 +0100

    Removed functionality from Class1,added to Repository/Processor

---
+++ LeagueAssist/MatchProcessor.cs
@@ -0,0 +1,56 @@
+﻿using LeagueAssist.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeagueAssist
+{
+    public class MatchProcessor
+    {
+        private IMatchRepository _matchRepository;
+
+        public IMatchRepository Repository
+        {
+            get { return _matchRepository; }
+            set { _matchRepository = value; }
+        }
+
+        public MatchProcessor()
+        {
+            _matchRepository = new MatchRepository();
+        }
+
+
+        public List<MatchReferees> RetrieveMatchesforReferee(int refereeId, int numberOfGamaes)
+        {
+            List < MatchReferees > result = _matchRepository.GetMatchesForReferee(refereeId, numberOfGamaes);
+            return result;
+        }
+
+        public MatchDetailInfo RetrieveMatchDetails (int matchId)
+        {
+            var stadium = _matchRepository.GetMatchStadiumInfo(matchId);
+            var matchActivity = _matchRepository.GetMatchActivityPlayers(matchId);
+            var playerList = _matchRepository.GetListOfPlayers(matchId);
+
+            MatchDetailInfo result = _matchRepository.GetMatchDetailInfo(stadium, matchActivity, playerList);
+            return result;
+        }
+
+        public string UpdateMatchInfo(int id, int HomeGoals, int AwayGoals, string Desc, List<MatchActions> lista)
+        {
+            string result = _matchRepository.UpdateMatch(id, HomeGoals, AwayGoals, Desc, lista);
+            return result;
+        }
+
+        public List<PlayerPerformance> GetPlayerPerformance()
+        {
+            List<PlayerPerformance> result = GetPlayerPerformance();
+            return result;
+
+        }
+
+    }
+}
