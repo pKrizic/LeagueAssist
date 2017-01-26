a834bdb4f726a7abdf8f06b9dbb1c888ba159b0e
Pavao <Pavao@ASUS_PC>
2017-01-26 22:55:08 +0100

    .

---
+++ LeagueAssist/Mappings/ListOfMatchMap.cs
@@ -0,0 +1,28 @@
+﻿using FluentNHibernate.Mapping;
+using LeagueAssist.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeagueAssist.Mappings
+{
+    public class ListOfMatchMap : ClassMap<ListOfMatch>
+    {
+        public ListOfMatchMap()
+        {
+            Id(x => x.Id);
+            Map(x => x.FirstOrg_Id);
+            Map(x => x.HomeName);
+            Map(x => x.FirstOrgScore);
+            Map(x => x.SecondOrg_Id);
+            Map(x => x.GuestName);
+            Map(x => x.SecondOrgScore);
+            Map(x => x.fixture_Id);
+            Map(x => x.DateTime);
+            Table("ListOfMatch");
+            ReadOnly();
+        }
+    }
+}
